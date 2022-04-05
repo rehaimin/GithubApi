08e1fe862934f44220a54e82499abe5ed4342f24
mohammedrehaimin <mrehaimin@gmail.com>
2022-04-05 16:47:22 +0000

    redesign

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ async function getData(url) {
 
 // Consommation
 let input = document.querySelector('input');
+const infos = document.querySelector('.info');
+
 
 document.querySelector('form').addEventListener('submit', (e) => {
     e.preventDefault();
@@ -19,11 +21,24 @@ document.querySelector('form').addEventListener('submit', (e) => {
                 img.src = data.avatar_url
                 let h5 = document.querySelector('h5');
                 h5.innerText = data.login + ' (' + data.location + ')'
-                h5.style.color = "gray";
+                h5.style.color = "#cccccc";
                 document.querySelector('h3').innerText = data.name
+                document.querySelector('a').href = "https://github.com/" + input.value
 
                 document.querySelector('.user-find').classList.remove('d-none')
             }
+        })
+        getData("https://api.github.com/users/" + input.value + "/repos").then((data) => {
+            // console.log(data);
+            infos.innerHTML = "<h3>Public Repos</h3>";
+            for (let i = 0; i < data.length; i++) {
+
+                if (!data[i].private || data[i].name != input.value) {
+                    infos.innerHTML = infos.innerHTML + "<p>" + data[i].name + "</p>"
+                }
+
+            }
+            document.querySelector('.info').classList.remove('d-none')
         })
     } else {
         // Message erreur
